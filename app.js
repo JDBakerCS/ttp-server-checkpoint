8ee652be06c32664204b3eaaf878ff895461b26f
James Dalton Baker <james.Baker59@stu.bmcc.cuny.edu>
2026-06-26 16:25:53 -0400

    finished with carenotes routes

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ let plants = [
 
 let nextId = 5;
 
+let careNotes = [
+    { id: 1, plantId: 1, text: "Water every 2 weeks" },
+    { id: 2, plantId: 1, text: "Keep away from pets" },
+    { id: 3, plantId: 3, text: "Mist leaves occasionally" },
+];
+
+let nextNoteId = 4;
+
 app.get("/api/plants", (req, res, next) => {
     try {
         const type = req.query.type;
@@ -35,24 +43,24 @@ app.get("/api/plants", (req, res, next) => {
 app.get("/api/plants/:id", async (req, res, next) => {
     try {
         const plantId = Number(req.params.id);
-        
-        const foundPlant = plants.find((plant) => plant.id ===plantId);
 
-    if (!foundPlant) {
-        return res.status(404).json({ error: "Plant not found"})
-    }
-    await new Promise((resolve) => 
-        setTimeout(resolve, 500));
+        const foundPlant = plants.find((plant) => plant.id === plantId);
 
-    res.json(foundPlant);
-    } catch(err) {
-        next(err); 
+        if (!foundPlant) {
+            return res.status(404).json({ error: "Plant not found" })
+        }
+        await new Promise((resolve) =>
+            setTimeout(resolve, 500));
+
+        res.json(foundPlant);
+    } catch (err) {
+        next(err);
     }
 })
 
 app.post("/api/plants", (req, res, next) => {
     try {
-        const {name, type, sunlight, watered} = req.body;
+        const { name, type, sunlight, watered } = req.body;
         const newPlant = {
             id: nextId,
             name,
@@ -63,7 +71,7 @@ app.post("/api/plants", (req, res, next) => {
         nextId++;
         plants.push(newPlant);
         res.status(201).json(newPlant)
-    } catch(err) {
+    } catch (err) {
         next(err)
     }
 })
@@ -74,27 +82,80 @@ app.patch("/api/plants/:id", (req, res, next) => {
         const foundPlant = plants.find((plant) => plant.id === plantId);
 
         if (!foundPlant) {
-            return res.status(404).json({error: "PLaNt No DER"})
+            return res.status(404).json({ error: "PLaNt No DER" })
         }
         Object.assign(foundPlant, req.body);
 
-        res.status(200).json(foundPlant) 
+        res.status(200).json(foundPlant)
     } catch (err) {
-        next(err); 
+        next(err);
     }
 })
 
 app.delete("/api/plants/:id", (req, res, next) => {
     try {
-    const plantId = Number(req.params.id);
-    const plantIndex = plants.findIndex((plant) => plant.id === plantId)
-    if (plantIndex === -1) {
-        return res.status(404).json({error: "plant says say less girrrl"})
-    }
-    plants.splice(plantIndex, 1);
+        const plantId = Number(req.params.id);
+        const plantIndex = plants.findIndex((plant) => plant.id === plantId)
+        if (plantIndex === -1) {
+            return res.status(404).json({ error: "plant says say less girrrl" })
+        }
+        plants.splice(plantIndex, 1);
 
-    res.sendStatus(204);
+        res.sendStatus(204);
+    } catch (err) {
+        next(err);
+    }
+})
+
+
+
+//CARENOTES ROUTES!!
+
+app.get("/api/plants/:plantId/notes", (req, res, next) => {
+    try {
+        const plantId = Number(req.params.id);
+
+        const foundPlant = plants.find((plant) => plant.id === plantId);
+
+        if (!plant) {
+            return res.status(404).json({error: "Plant Not Found"})
+        }
+
+        const notesForPlant = careNotes.filter((notes) => notes.plantId === plantId)
+
+        res.json(notesForPlant);
     } catch(err) {
+        next (err);
+    }
+})
+app.post("/api/plants/:plantId/notes", (req, res, next) => {
+    try {
+        const {text} = req.body;
+        const newNotes = {
+            id: nextNoteId,
+            plantId,
+            text,
+        }
+        nextNoteId++;
+        careNotes.push(newNotes);
+        res.status(201).json(newNotes)
+    } catch (err) {
+        next(err);
+    }
+})
+app.delete("/api/plants/:plantId/notes", (req, res, next) => {
+    try {
+        const notesId = Number(req.params.id);
+        const plantId = Number(req.params.id);
+
+        const notesIndex = careNotes.findIndex((note) => note.id === notesId && note.plantId ===plantId);
+
+        if (notesIndex === -1) {
+            return res.status(404).json({error: "thouith spilt thine tea"})
+        }
+        careNotes.splice(notesIndex, 1);
+        res.sendStatus(204);
+    } catch (err) {
         next(err);
     }
 })
